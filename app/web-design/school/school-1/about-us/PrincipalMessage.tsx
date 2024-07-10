42a2b9398a68cbfbd148d26279cdde68f12b4c25
Mkhotamirais <mkhotamirais@gmail.com>
2024-07-10 17:59:52 +0700

    cuma masalha apos

--- app/web-design/school/school-1/about-us/PrincipalMessage.tsx
+++ app/web-design/school/school-1/about-us/PrincipalMessage.tsx
@@ -1,7 +1,7 @@
 export default function PrincipalMessage() {
   return (
     <div>
-      Principal's Message <br />
+      Principal&apos;s Message <br />
       Welcome to Bluefield Highschool! Our school is a place where students are encouraged to dream big and work hard to
       achieve their goals. We believe in providing a well-rounded education that not only focuses on academic success
       but also on developing strong moral values and leadership skills. I am proud to be part of a community that is
@@ -9,7 +9,7 @@ export default function PrincipalMessage() {
       our school.
       <br />
       Sincerely, <br />
-      [Principal's Name] <br />
+      [Principal&apos;s Name] <br />
       Principal, Bluefield Highschool
     </div>
   );
